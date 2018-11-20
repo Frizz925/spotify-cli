69ff4d5a04960f0d26ebda7cee947db404c70e66
Izra Faturrahman <Frizz925@hotmail.com>
2018-11-21 02:22:50 +0700

    Rename command_line.py to cli.py

--- spotify/cli.py
+++ spotify/cli.py
@@ -1,4 +1,5 @@
-from spotify import get_authorization_code, get_access_token, get_current_playing, format_current_playing
+from . import get_authorization_code, get_access_token, refresh_access_token, \
+    get_current_playing, format_current_playing
 
 import click
 import json
@@ -8,16 +9,21 @@ import sys
 @click.command()
 def main():
     credentials = load_credentials()
+    client_id = credentials['client_id']
+    client_secret = credentials['client_secret']
     if not 'access_token' in credentials:
         logerr('Access token not found. Opening browser for authorization.')
-        client_id = credentials['client_id']
-        client_secret = credentials['client_secret']
         code = get_authorization_code(client_id)
         access_token = get_access_token(client_id, client_secret, code)
         credentials['access_token'] = access_token
         save_credentials(credentials)
     else:
         access_token = credentials['access_token']
+        new_access_token = refresh_access_token(client_id, client_secret, access_token)
+        if new_access_token is not None:
+            access_token = new_access_token
+            credentials['access_token'] = access_token
+            save_credentials(credentials)
     current_playing = get_current_playing(access_token)
     if current_playing is None:
         logerr('Not currently playing any track.')
