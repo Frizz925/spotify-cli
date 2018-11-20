79a3c0fceae5b8361cbc9f3fb5bbbc812f1237e1
Izra Faturrahman <Frizz925@hotmail.com>
2018-11-21 02:23:15 +0700

    Update setup script

---
+++ run.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from spotify import cli
+import sys
+
+if __name__ == '__main__':
+    sys.exit(cli.main())
--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup(name='Spotify CLI',
-      version='1.0',
-      description='Access your Spotify data through CLI',
-      author='Izra Faturrahman',
-      author_email='Frizz925@hotmail.com',
-      scripts=['spotify/command_line.py'],
-      packages=['spotify']
-)
+
+def read_requirements(filename):
+    with open(filename, 'r') as f:
+        reqs = f.read().splitlines()
+    return reqs
+
+
+setup(**{
+    'name': 'Spotify CLI',
+    'version': '1.0',
+    'description': 'Access your Spotify data through CLI',
+    'author': 'Izra Faturrahman',
+    'author_email': 'Frizz925@hotmail.com',
+    'entry_points': {
+        'console_scripts': [
+            'spotify = spotify.cli:main'
+        ]
+    },
+    'packages': ['spotify'],
+    'install_requires': read_requirements('requirements.txt')
+})
